e323a3060e56737e245053ea2b92f0ad607134d4
Moha2001-dev <80723154+Moha2001-dev@users.noreply.github.com>
2023-08-14 00:09:10 +0300

    Refactor Code Clean the Code Add Comments

--- lib/models/fms_file.dart
+++ lib/models/fms_file.dart
@@ -1,45 +1,53 @@
-import 'package:hijri/hijri_calendar.dart';
 
+///A [FMSFile] class holds a real life file's(معاملة) object data
 class FMSFile {
   String? _title;
   String? _date;
   String _fileNum = "";
   String _path = "";
 
+  ///initialize [FMSFile] object by data passing.
   FMSFile({String? title, String? date, required String fileNum, required String path})
-      : this._title = title,
-        this._date = date,
-        this._fileNum = fileNum,
-        this._path = path;
+      : _title = title,
+        _date = date,
+        _fileNum = fileNum,
+        _path = path;
 
+  ///initialize [FMSFile] object by data directly fetched from database.
   FMSFile.fmsDB(Map<dynamic,dynamic> query){
-    this.fileNum = query['filenumber'];
-    this.path = query['filepath'];
-    List<String> dateList = query['date'].toString().split('-');
-    DateTime dateTime = DateTime(int.parse(dateList[0]),int.parse(dateList[1]),int.parse(dateList[2]));
-    this._date = (query['date'] == '1010-01-01')? "" : HijriCalendar.fromDate(dateTime).toString();
-    this.title = (query['title'] == 'empty')? "" : query['title'];
+    fileNum = query['filenumber'];
+    path = query['filepath'];
+    _date = (query['date'] == '1010-01-01')? "" : DateTime(int.parse(query['date'].toString().split(RegExp(r'-'))[0]),int.parse(query['date'].toString().split(RegExp(r'-'))[1]),int.parse(query['date'].toString().split(RegExp(r'-'))[2])).toString();
+    title = (query['title'] == 'empty')? "" : query['title'];
   }
 
-  String? get title => _title;
+  String? get title {
+    return _title;
+  }
 
   set title(String? value) {
     _title = value;
   }
 
-  String? get date => _date;
+  String? get date {
+    return _date;
+  }
 
   set date(String? value) {
     _date = value;
   }
 
-  String get path => _path;
+  String get path {
+    return _path;
+  }
 
   set path(String value) {
     _path = value;
   }
 
-  String get fileNum => _fileNum;
+  String get fileNum {
+    return _fileNum;
+  }
 
   set fileNum(String value) {
     _fileNum = value;
